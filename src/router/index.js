96add546b669ad6e2b0e4cd2ca42a2a966884713
Akshit Khanna <akshit201211c@gmail.com>
2022-04-27 22:53:55 +0530

    search github profiles initial work

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,53 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import SignIn from "../views/Signin.vue"
+import SignUp from "../views/Signup.vue"
+import PageNotFound from "../views/PageNotFound.vue"
+
+import store from "../store"
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: Home,
+    meta: {
+      auth: true //A protected route
+    }
   },
   {
     path: '/about',
-    name: 'about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/signin',
+    name: 'SignIn',
+    component: SignIn
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {path: '/:catchAll(.*)', component: PageNotFound},
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  console.log({to});
+  if (to.meta.auth && !store.getters.isAuthenticated) {
+    next('/signin')
+  } else {
+    next()
+  }
 })
 
 export default router
---
+++ src/utils/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC5hLCaJlPiYPWA4T7SKMoaN6p-WB_nn58",
+  authDomain: "git-profiler-ad50d.firebaseapp.com",
+  projectId: "git-profiler-ad50d",
+  storageBucket: "git-profiler-ad50d.appspot.com",
+  messagingSenderId: "170948370441",
+  appId: "1:170948370441:web:2e5ff8a28904bd225398d8"
+  };
+
+  export default firebaseConfig
